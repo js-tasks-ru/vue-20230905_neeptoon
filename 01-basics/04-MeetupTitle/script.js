fd717d5cdba6b1f8bf093010c57835eff172354e
Evgeniy Shekhanov <eshekhanov@phoenixit.ru>
2023-08-29 09:33:56 +0300

    добавляет решение MeetupTitle

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -13,5 +13,33 @@ function fetchMeetupById(meetupId) {
     }
   });
 }
+
+createApp({
+  data() {
+    return {
+      currentMeetupId: '',
+      meetup: '',
+    };
+  },
+
+  watch: {
+    async currentMeetupId(value) {
+      this.meetup = await fetchMeetupById(value);
+    },
+  },
+
+  template: `
+  <div>
+    <label><input type="radio" name="meetupId" value="1" v-model="currentMeetupId"/> 1</label>
+    <label><input type="radio" name="meetupId" value="2" v-model="currentMeetupId"/> 2</label>
+    <label><input type="radio" name="meetupId" value="3" v-model="currentMeetupId"/> 3</label>
+    <label><input type="radio" name="meetupId" value="4" v-model="currentMeetupId"/> 4</label>
+    <label><input type="radio" name="meetupId" value="5" v-model="currentMeetupId"/> 5</label>
+
+    <hr />
+
+    <h3>{{ meetup.title }}</h3>
+  </div>`,
+}).mount('#app');
 
 // Требуется создать Vue приложение
