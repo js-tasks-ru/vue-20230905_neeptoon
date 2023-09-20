22ebb59e0e063eee42a4364ee07fd64904738b1d
Evgeniy Shekhanov <eshekhanov@phoenixit.ru>
2023-09-20 16:16:12 +0300

    добавляет решение задачи  MeetupInfo

--- 02-components/04-MeetupInfo/components/MeetupInfo.js
+++ 02-components/04-MeetupInfo/components/MeetupInfo.js
@@ -3,19 +3,48 @@ import { defineComponent } from '../vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'MeetupInfo',
 
+  props: {
+    organizer: {
+      required: true,
+      type: String,
+    },
+    place: {
+      required: true,
+      type: String,
+    },
+    date: {
+      required: true,
+      type: Number,
+    },
+  },
+
+  methods: {
+    formatAsLocaleDate(timestamp) {
+      return new Date(timestamp).toLocaleDateString(navigator.language, {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      });
+    },
+
+    formatAsISODate(timestamp) {
+      return new Date(timestamp).toISOString().substring(0, 10);
+    },
+  },
+
   template: `
     <ul class="meetup-info">
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{ organizer }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{ place }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time :datetime="formatAsISODate(date)">{{ formatAsLocaleDate(date) }}</time>
       </li>
     </ul>`,
 });
